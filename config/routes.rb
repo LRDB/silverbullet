cb6b2f4af037ed75082ce3f963653faddf8cfcee
LRDB <ldbassig@up.edu.ph>
2025-02-17 06:40:31 +0800

    feat: finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root    "home#home"
-  get     "/home",       to: "home#home"
+  get     "/home",        to: "home#home"
+  get     "/signup",      to: "users#new"
 end
