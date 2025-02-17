fa222afc4f8247be7e66baa4c49cba95fa1a014e
LRDB <ldbassig@up.edu.ph>
2025-02-17 09:04:25 +0800

    feat: add login func

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get "sessions/new"
   root      "home#home"
   get       "/home",        to: "home#home"
   get       "/signup",      to: "users#new"
+  get       "/login",       to: "sessions#new"
+  post      "/login",       to: "sessions#create"
+  get       "/logout",      to: "sessions#destroy"
   resources :users
 end
