cb50eaa9934ec68bb097023522964b65e2fcf28d
LRDB <ldbassig@up.edu.ph>
2025-02-17 08:17:29 +0800

    feat: add flash messages for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = "Account created."
+      redirect_to @user
     else
-      render 'new'
+      flash[:danger] = @user.errors.full_messages
+      redirect_to signup_url
     end
   end
 
